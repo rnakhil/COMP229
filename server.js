32ba35d32b2068837b911f7c9d4800263acc172e
rnakhil <walkingwithmyfriend@gmail.com>
2023-09-27 11:30:22 -0400

    added PORT to server.js for heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
+const PORT = process.env.PORT || 3000
 var app = express();
-app.listen(3000);
+app.listen(PORT);
 module.exports = app;
 
 console.log('Server running at http://localhost:3000/');
